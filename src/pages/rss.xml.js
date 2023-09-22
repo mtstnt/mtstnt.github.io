a257cf7fe2f75486608a504671503b65ad266a36
mtstnt <matthew.sutanto0612@gmail.com>
2023-09-23 01:50:16 +0700

    fixed consts

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,11 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
 	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
+		title: "Matthew Sutanto Blog",
+		description: "Tech posts",
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
